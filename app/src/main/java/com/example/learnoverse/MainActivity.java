e2a6c73860484a0e484b28353c9fb8506f3dd7b6
rohithgoud20 <rohithmithra16@gmail.com>
2023-10-27 18:32:58 -0400

    singup

--- app/src/main/java/com/example/learnoverse/MainActivity.java
+++ app/src/main/java/com/example/learnoverse/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.learnoverse;
 
+import static com.example.learnoverse.R.id.button2;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSignUp=findViewById(R.id.button2);
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,signup.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/learnoverse/signup.java
+++ app/src/main/java/com/example/learnoverse/signup.java
@@ -3,12 +3,44 @@ package com.example.learnoverse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class signup extends AppCompatActivity {
+
+    private Spinner spinnerInterests;
+    private ArrayAdapter<CharSequence> adapter;
+    private EditText editTextName;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonSignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+        spinnerInterests = findViewById(R.id.spinnerInterests);
+        adapter = ArrayAdapter.createFromResource(this, R.array.interests_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerInterests.setAdapter(adapter);
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle sign-up logic here
+                String name = editTextName.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                // Add your logic for processing the sign-up action
+            }
+        });
     }
 }
