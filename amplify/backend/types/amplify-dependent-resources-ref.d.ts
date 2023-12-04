625cd69b3e0793bb7abcc572f6e9ddc3be2347b8
rohithgoud20 <rohithmithra16@gmail.com>
2023-12-03 21:11:41 -0500

    half

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "learnoverse4eae4764": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3be20a5cc": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
